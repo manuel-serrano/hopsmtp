80c4405db758f0c9315cb3b61bd38ccbc98e5d28
Manuel Serrano <Manuel.Serrano@inria.fr>
2021-12-22 10:57:38 +0100

    .

--- cdb.js
+++ cdb.js
@@ -3,43 +3,42 @@
 /*    -------------------------------------------------------------    */
 /*    Author      :  Manuel Serrano                                    */
 /*    Creation    :  Wed Jan  6 15:20:35 2021                          */
-/*    Last change :  Wed Jan  6 15:33:49 2021 (serrano)                */
+/*    Last change :  Wed Dec 22 10:41:23 2021 (serrano)                */
 /*    Copyright   :  2021 Manuel Serrano                               */
 /*    -------------------------------------------------------------    */
 /*    Contact database (for encryption and for smtp server selection)  */
 /*=====================================================================*/
-const fs = require( 'fs' );
-const path = require( 'path' );
-
-import { load } from hop.vcf;
+import fs from 'fs';
+import path from 'path';
+import { load } from 'hop:vcf';
 
 /*---------------------------------------------------------------------*/
 /*    loadCDB ...                                                      */
 /*---------------------------------------------------------------------*/
-export function loadCDB( files ) {
-   for( let i = 0; i < files.length; i++ ) {
+export function loadCDB(files) {
+   for (let i = 0; i < files.length; i++) {
       const f = files[ i ];
       
-      if( fs.existsSync( f ) ) {
+      if (fs.existsSync(f)) {
 	 try {
-	    return load( f );
-	 } catch( e ) {
-	    console.error( "hopsmtp: cannot load cdb \"" + f + "\"" );
-	    console.error( e.toString() );
+	    return load(f);
+	 } catch (e) {
+	    console.error("hopsmtp: cannot load cdb \"" + f + "\"");
+	    console.error(e.toString());
 	 }
       } else {
-	 const m = f.match( /([$]HOME)[/](.*)/ );
+	 const m = f.match(/([$]HOME)[/](.*)/);
 
-	 if( m ) {
-	    const g = path.join( process.env.HOME, m[ 2 ] );
+	 if (m) {
+	    const g = path.join(process.env.HOME, m[ 2 ]);
 	    
-	    if( fs.existsSync( g ) ) {
+	    if (fs.existsSync(g)) {
 	       try {
-	       	  return load( g );
-	       } catch( e ) {
-	    	  console.error( "hopsmtp: cannot load cdb \"" + g + "\"" );
-		  #:exception-notify( e );
-	    	  console.error( e.toString() );
+	       	  return load(g);
+	       } catch (e) {
+	    	  console.error("hopsmtp: cannot load cdb \"" + g + "\"");
+		  #:exception-notify(e);
+	    	  console.error(e.toString());
 	       }
 	    }
 	 }
@@ -51,8 +50,7 @@ export function loadCDB( files ) {
 /*---------------------------------------------------------------------*/
 /*    findCDB ...                                                      */
 /*---------------------------------------------------------------------*/
-export function findCDB( cdb, email ) {
-   return cdb.find( contact => 
-	 contact.emails.some( e => 
-			  e.email.indexOf( email ) >= 0 ) );
+export function findCDB(cdb, email) {
+   return cdb.find(contact => 
+		contact.emails.some(e => e.email.indexOf(email) >= 0));
 }
